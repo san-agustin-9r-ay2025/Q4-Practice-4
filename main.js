51d0167941cd0ffcb2ff75a99a2d55871c731d8a
san-agustin-9r-ay2025 <sanagustin.go@obmontessori.edu.ph>
2026-04-17 12:53:43 +0800

    Finished

---
+++ main.js
@@ -0,0 +1,10 @@
+function openSesimei() {
+    let counter = 18;
+    const maxCounts = 25;
+
+    while (counter < maxCounts) {
+        window.alert(counter);
+        counter += 1;
+    }
+    window.alert("Full parking!");
+}
